7711de85bedf268ec7c71b537c6012f7d66a96ac
David Robert <contact.davidrobert@gmail.com>
2021-11-30 16:20:07 +0100

    extends twig

---
+++ src/Controller/PageController.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PageController extends AbstractController
+{
+
+    /**
+     * @Route("/", name="home")
+     */
+    public function home()
+    {
+        return $this->render('base.html.twig');
+    }
+
+}
