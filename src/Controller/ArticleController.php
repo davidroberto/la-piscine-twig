e79fe33848a1cb6cf223072a6f8cb6048862e690
David Robert <contact.davidrobert@gmail.com>
2021-11-30 13:40:17 +0100

    not found exception

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -5,35 +5,61 @@ namespace App\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController extends AbstractController
 {
 
     /**
-     * @Route("/article", name="article_show")
+     * @Route("/article/{id}", name="article_show")
      */
-    public function ArticleShow()
+    public function ArticleShow($id)
     {
-        // simulation d'une requête en base de données pour
-        // récupérer un article et le stocker dans une variable
-        $article = [
-            "title" => "Le PHP c'est vraiment trop génial",
-            "content" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta et, facilis laboriosam tenetur unde vero! Ad consequatur, debitis dolorum eos error explicabo id, iusto magni nam odio quas reprehenderit ut.",
-            "image" => "https://static1.purepeople.com/articles/7/26/58/37/@/3758746-exclusif-christian-clavier-enregistr-624x600-2.jpg",
-            "isPublished" => false
+        $articles = [
+            1 => [
+                "title" => "Le PHP c'est vraiment trop génial",
+                "content" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta et, facilis laboriosam tenetur unde vero! Ad consequatur, debitis dolorum eos error explicabo id, iusto magni nam odio quas reprehenderit ut.",
+                "image" => "https://static1.purepeople.com/articles/7/26/58/37/@/3758746-exclusif-christian-clavier-enregistr-624x600-2.jpg",
+                "isPublished" => false,
+                "id" => 1
+            ],
+            2 => [
+                "title" => "Le PHP c'est vraiment trop génial 2",
+                "content" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta et, facilis laboriosam tenetur unde vero! Ad consequatur, debitis dolorum eos error explicabo id, iusto magni nam odio quas reprehenderit ut.",
+                "image" => "https://static1.purepeople.com/articles/7/26/58/37/@/3758746-exclusif-christian-clavier-enregistr-624x600-2.jpg",
+                "isPublished" => true,
+                "id" => 2
+            ],
+            3 => [
+                "title" => "Le PHP c'est vraiment trop génial 3",
+                "content" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta et, facilis laboriosam tenetur unde vero! Ad consequatur, debitis dolorum eos error explicabo id, iusto magni nam odio quas reprehenderit ut.",
+                "image" => "https://static1.purepeople.com/articles/7/26/58/37/@/3758746-exclusif-christian-clavier-enregistr-624x600-2.jpg",
+                "isPublished" => true,
+                "id" => 3
+            ],
+            4 => [
+                "title" => "Le PHP c'est vraiment trop génial 4",
+                "content" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta et, facilis laboriosam tenetur unde vero! Ad consequatur, debitis dolorum eos error explicabo id, iusto magni nam odio quas reprehenderit ut.",
+                "image" => "https://static1.purepeople.com/articles/7/26/58/37/@/3758746-exclusif-christian-clavier-enregistr-624x600-2.jpg",
+                "isPublished" => false,
+                "id" => 4
+            ],
+            5 => [
+                "title" => "Le PHP c'est vraiment trop génial 5",
+                "content" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta et, facilis laboriosam tenetur unde vero! Ad consequatur, debitis dolorum eos error explicabo id, iusto magni nam odio quas reprehenderit ut.",
+                "image" => "https://static1.purepeople.com/articles/7/26/58/37/@/3758746-exclusif-christian-clavier-enregistr-624x600-2.jpg",
+                "isPublished" => true,
+                "id" => 5
+            ]
         ];
 
-        // je récupère un fichier twig, je le compile en HTML
-        // grâce à la méthode render
-        // je lui passe la variable article pour qu'il puisse
-        // l'utiliser.
-
-        // et je renvoie le HTML généré
-        // en réponse HTTP au navigateur
+        if (!array_key_exists($id, $articles)) {
+            throw $this->createNotFoundException("L'article n'a pas été trouvé !");
+        }
 
         return $this->render('article.html.twig', [
-            'article' => $article
+            'article' => $articles[$id]
         ]);
     }
 
@@ -44,35 +70,40 @@ class ArticleController extends AbstractController
     public function articleList()
     {
         $articles = [
-            [
+            1 => [
                 "title" => "Le PHP c'est vraiment trop génial",
                 "content" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta et, facilis laboriosam tenetur unde vero! Ad consequatur, debitis dolorum eos error explicabo id, iusto magni nam odio quas reprehenderit ut.",
                 "image" => "https://static1.purepeople.com/articles/7/26/58/37/@/3758746-exclusif-christian-clavier-enregistr-624x600-2.jpg",
-                "isPublished" => false
+                "isPublished" => false,
+                "id" => 1
             ],
-            [
+            2 => [
                 "title" => "Le PHP c'est vraiment trop génial 2",
                 "content" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta et, facilis laboriosam tenetur unde vero! Ad consequatur, debitis dolorum eos error explicabo id, iusto magni nam odio quas reprehenderit ut.",
                 "image" => "https://static1.purepeople.com/articles/7/26/58/37/@/3758746-exclusif-christian-clavier-enregistr-624x600-2.jpg",
-                "isPublished" => true
+                "isPublished" => true,
+                "id" => 2
             ],
-            [
+            3 => [
                 "title" => "Le PHP c'est vraiment trop génial 3",
                 "content" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta et, facilis laboriosam tenetur unde vero! Ad consequatur, debitis dolorum eos error explicabo id, iusto magni nam odio quas reprehenderit ut.",
                 "image" => "https://static1.purepeople.com/articles/7/26/58/37/@/3758746-exclusif-christian-clavier-enregistr-624x600-2.jpg",
-                "isPublished" => true
+                "isPublished" => true,
+                "id" => 3
             ],
-            [
+            4 => [
                 "title" => "Le PHP c'est vraiment trop génial 4",
                 "content" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta et, facilis laboriosam tenetur unde vero! Ad consequatur, debitis dolorum eos error explicabo id, iusto magni nam odio quas reprehenderit ut.",
                 "image" => "https://static1.purepeople.com/articles/7/26/58/37/@/3758746-exclusif-christian-clavier-enregistr-624x600-2.jpg",
-                "isPublished" => false
+                "isPublished" => false,
+                "id" => 4
             ],
-            [
+            5 => [
                 "title" => "Le PHP c'est vraiment trop génial 5",
                 "content" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta et, facilis laboriosam tenetur unde vero! Ad consequatur, debitis dolorum eos error explicabo id, iusto magni nam odio quas reprehenderit ut.",
                 "image" => "https://static1.purepeople.com/articles/7/26/58/37/@/3758746-exclusif-christian-clavier-enregistr-624x600-2.jpg",
-                "isPublished" => true
+                "isPublished" => true,
+                "id" => 5
             ]
         ];
 
