75605306f7a94aaaab0ee8d6c06bf3011e1d3b31
David Robert <contact.davidrobert@gmail.com>
2021-12-02 14:40:53 +0100

    constructor & visibility

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -10,13 +10,19 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController extends AbstractController
 {
+    // je créé une propriété privé $articles
+    // qui contiendra la liste de tous les articles
+    // afin d'éviter de répéter cette variable dans toutes les méthodes
+    private $articles;
 
-    /**
-     * @Route("/article/{id}", name="article_show")
-     */
-    public function ArticleShow($id)
+    // je créé une méthode constructor, qui sera appelée automatiquement
+    // quand la classe sera instanciée
+    // Symfony instancie les classes de controller (même si on le voit pas)
+    // pour afficher les pages créées dans le controleur
+    public function __construct()
     {
-        $articles = [
+        // je définis la valeur de la propriété article
+        $this->articles = [
             1 => [
                 "title" => "Le PHP c'est vraiment trop génial",
                 "content" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta et, facilis laboriosam tenetur unde vero! Ad consequatur, debitis dolorum eos error explicabo id, iusto magni nam odio quas reprehenderit ut.",
@@ -58,62 +64,29 @@ class ArticleController extends AbstractController
                 "id" => 5
             ]
         ];
+    }
 
-        if (!array_key_exists($id, $articles)) {
+    /**
+     * @Route("/article/{id}", name="article_show")
+     */
+    public function articleShow($id)
+    {
+        if (!array_key_exists($id, $this->articles)) {
             throw $this->createNotFoundException("L'article n'a pas été trouvé !");
         }
 
         return $this->render('article.html.twig', [
-            'article' => $articles[$id]
+            'article' => $this->articles[$id]
         ]);
     }
-
 
     /**
      * @Route("/articles", name="article_list")
      */
     public function articleList()
     {
-        $articles = [
-            1 => [
-                "title" => "Le PHP c'est vraiment trop génial",
-                "content" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta et, facilis laboriosam tenetur unde vero! Ad consequatur, debitis dolorum eos error explicabo id, iusto magni nam odio quas reprehenderit ut.",
-                "image" => "https://static1.purepeople.com/articles/7/26/58/37/@/3758746-exclusif-christian-clavier-enregistr-624x600-2.jpg",
-                "isPublished" => false,
-                "id" => 1
-            ],
-            2 => [
-                "title" => "Le PHP c'est vraiment trop génial 2",
-                "content" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta et, facilis laboriosam tenetur unde vero! Ad consequatur, debitis dolorum eos error explicabo id, iusto magni nam odio quas reprehenderit ut.",
-                "image" => "https://static1.purepeople.com/articles/7/26/58/37/@/3758746-exclusif-christian-clavier-enregistr-624x600-2.jpg",
-                "isPublished" => true,
-                "id" => 2
-            ],
-            3 => [
-                "title" => "Le PHP c'est vraiment trop génial 3",
-                "content" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta et, facilis laboriosam tenetur unde vero! Ad consequatur, debitis dolorum eos error explicabo id, iusto magni nam odio quas reprehenderit ut.",
-                "image" => "https://static1.purepeople.com/articles/7/26/58/37/@/3758746-exclusif-christian-clavier-enregistr-624x600-2.jpg",
-                "isPublished" => true,
-                "id" => 3
-            ],
-            4 => [
-                "title" => "Le PHP c'est vraiment trop génial 4",
-                "content" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta et, facilis laboriosam tenetur unde vero! Ad consequatur, debitis dolorum eos error explicabo id, iusto magni nam odio quas reprehenderit ut.",
-                "image" => "https://static1.purepeople.com/articles/7/26/58/37/@/3758746-exclusif-christian-clavier-enregistr-624x600-2.jpg",
-                "isPublished" => false,
-                "id" => 4
-            ],
-            5 => [
-                "title" => "Le PHP c'est vraiment trop génial 5",
-                "content" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta et, facilis laboriosam tenetur unde vero! Ad consequatur, debitis dolorum eos error explicabo id, iusto magni nam odio quas reprehenderit ut.",
-                "image" => "https://static1.purepeople.com/articles/7/26/58/37/@/3758746-exclusif-christian-clavier-enregistr-624x600-2.jpg",
-                "isPublished" => true,
-                "id" => 5
-            ]
-        ];
-
         return $this->render('articles.html.twig', [
-            'articles' => $articles
+            'articles' => $this->articles
         ]);
     }
 
